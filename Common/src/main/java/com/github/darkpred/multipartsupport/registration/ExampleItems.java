e199097e52adfd845fb0b129ec9a74a2da6b1b82
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-09-18 00:37:13 +0200

    Initial commit

---
+++ Common/src/main/java/com/github/darkpred/multipartsupport/registration/ExampleItems.java
@@ -0,0 +1,22 @@
+package com.github.darkpred.multipartsupport.registration;
+
+import com.github.darkpred.multipartsupport.Constants;
+import net.minecraft.core.Registry;
+import net.minecraft.world.item.Item;
+
+/**
+ * Example class for item registration
+ */
+public class ExampleItems {
+
+    /**
+     * The provider for items
+     */
+    public static final RegistrationProvider<Item> ITEMS = RegistrationProvider.get(Registry.ITEM_REGISTRY, Constants.MOD_ID);
+
+    public static final RegistryObject<Item> EXAMPLE = ITEMS.register("example", () -> new Item(new Item.Properties().fireResistant().stacksTo(12)));
+
+    // Called in the mod initializer / constructor in order to make sure that items are registered
+    public static void loadClass() {
+    }
+}
